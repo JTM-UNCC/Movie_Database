f23dca44c06423111f45500bc2b1a84d9d2b5bc6
JT Madden <143136330+JTM-UNCC@users.noreply.github.com>
2024-11-21 18:46:58 -0500

    Update procedures.sql to add Remove and IsIn procedure

--- MovieWatchlistDatabase/sql/procedures.sql
+++ MovieWatchlistDatabase/sql/procedures.sql
@@ -26,4 +26,31 @@ BEGIN
     WHERE w.user_id = userId;
 END;
 
+-- Removes a movie from watchlist --
+// DELIMITER ;
+DELIMITER //
+CREATE PROCEDURE RemoveFromWatchlist (
+    IN userId INT,
+    IN movieId INT
+)
+BEGIN
+    DELETE FROM Watchlist
+    WHERE user_id = userId AND movie_id = movieId;
+END;
+// DELIMITER ;
+
+-- Checks if movie is in watchlist --
+DELIMITER //
+CREATE PROCEDURE IsInWatchlist (
+    IN userId INT,
+    IN movieId INT,
+    OUT isIn BOOLEAN
+)
+BEGIN
+    SELECT EXISTS(
+        SELECT 1 
+        FROM Watchlist
+        WHERE user_id = userId AND movie_id = movieId
+    ) INTO isIn;
+END;
 // DELIMITER ;
