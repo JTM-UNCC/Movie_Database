56b2a130e06728ad87cb4aab49a148a99ea235e8
Jplumm20 <jplumm20@uncc.edu>
2024-11-25 11:13:50 -0500

    Update data.sql

--- MovieWatchlistDatabase/sql/data.sql
+++ MovieWatchlistDatabase/sql/data.sql
@@ -5,11 +5,19 @@ VALUES
     ('The Matrix', 'Sci-Fi', 1999, 'Lana Wachowski'),
     ('Titanic', 'Romance', 1997, 'James Cameron'),
     ('Toy Story', 'Animation', 1995, 'John Lasseter');
+    ('Avatar', 'Sci-Fi', 2009, 'James Cameron'),
+    ('The Dark Knight', 'Action', 2008, 'Christopher Nolan'),
+    ('The Shawshank Redemption', 'Drama', 1994, 'Frank Darabont'),
+    ('The Conjuring', 'Horror', 2013, 'James Wan'),
+    ('Parasite', 'Thriller', 2019, 'Bong Joon Ho');
+
 
 INSERT INTO Users (username, email)
 VALUES 
-    ('alice', 'alice@example.com'),
-    ('bob', 'bob@example.com');
+    ('admin', 'admin@example.com', 'Admin'),
+    ('alice', 'alice@example.com', 'User'),
+    ('bob', 'bob@example.com', 'User');
+    
 
 INSERT INTO Watchlist (user_id, movie_id, status)
 VALUES 
